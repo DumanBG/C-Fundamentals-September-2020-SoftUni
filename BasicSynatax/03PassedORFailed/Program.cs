0ffdad48078255e729c93afdf6c29598a8eb7d94
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 14:09:24 +0200

    Add files via upload

---
+++ BasicSynatax/03PassedORFailed/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gradeInput = double.Parse(Console.ReadLine());
+
+            if (gradeInput >= 3)
+            {
+                Console.WriteLine("Passed!");
+            }
+            else
+            {
+                Console.WriteLine("Failed!");
+            }
+        }
+    }
+}
---
+++ BasicSynatax/Passed02/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Passed02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double gradeInput = double.Parse(Console.ReadLine());
+
+            if(gradeInput>=3)
+            {
+                Console.WriteLine("Passed!");
+            }
+
+
+        }
+    }
+}
