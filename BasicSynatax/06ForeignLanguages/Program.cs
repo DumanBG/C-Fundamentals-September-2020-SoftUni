4ef7a033679061978d25d924c7e3cb2b77820533
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 14:15:14 +0200

    Add files via upload

---
+++ BasicSynatax/06ForeignLanguages/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _06ForeignLanguages
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string language = Console.ReadLine();
+
+
+            switch (language)
+            {
+                case "England":
+                case "USA":
+                    Console.WriteLine("English");
+                    break;
+                case "Spain":
+                case "Argentina":
+                case "Mexico":
+                    Console.WriteLine("Spanish");
+                    break;
+                default:
+                    Console.WriteLine("unknown");
+                    break;
+
+
+
+            }
+        }
+    }
+}
---
+++ BasicSynatax/07TheatrePromotion/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+
+namespace _07TheatrePromotion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string day = Console.ReadLine();
+            int age = int.Parse(Console.ReadLine());
+
+            int price = 0;
+
+            if (age < 0 || age > 122)
+            {
+                Console.WriteLine("Error!");
+                return;
+            }
+
+
+            switch (day)
+            {
+                case "Weekday":  
+                    if(age<=18)
+                    {
+                        price = 12;
+
+                    }
+                    else if (age <= 64)
+                    {
+                        price = 18;
+                    }
+                    else if (age <= 122)
+                    {
+                        price = 12;
+
+                    }
+                    break;
+
+                case "Weekend":
+                    if (age <= 18)
+                    {
+                        price = 15;
+
+                    }
+                    else if (age <= 64)
+                    {
+                        price = 20;
+                    }
+                    else if (age <= 122)
+                    {
+                        price = 15;
+
+                    }
+                    break;
+
+                case "Holiday":
+                    if (age <= 18)
+                    {
+                        price = 5;
+
+                    }
+                    else if (age <= 64)
+                    {
+                        price = 12;
+                    }
+                    else if (age <= 122)
+                    {
+                        price = 10;
+
+                    }
+                    break;
+            }
+
+            Console.WriteLine($"{price}$");
+        }
+    }
+}
