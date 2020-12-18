e8f2cb34c428bb07ae8672d568523a632fb62364
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 17:06:43 +0200

    Add files via upload

---
+++ Final Exam/01.PasswordResetStringOperational/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.PasswordResetStringOperational
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password = Console.ReadLine();
+            string input = Console.ReadLine();
+
+            while (input != "Done")
+            {
+                string[] tokens = input.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                string cmdArg = tokens[0];
+                switch (cmdArg)
+                {
+                    case "TakeOdd":
+                        string newString = "";
+                        // Вземане само на ODD symbols from string
+                        newString = string.Concat(password.Where((c, i) => i % 2 != 0));
+                        password = newString;
+                        Console.WriteLine(password);
+                        break;
+
+                    case "Cut":
+                        //•	Cut {index} {length}
+                        int index = int.Parse(tokens[1]);
+                        int length = int.Parse(tokens[2]);
+                        string cutted = password.Substring(index, length);
+                        int FirstGet = password.IndexOf(cutted);
+                        password = password.Remove(FirstGet, length);
+                         Console.WriteLine(password);
+
+                        break;
+                    case "Substitute":
+                        //   	Substitute { substring} { substitute}  || Substitute :: -
+                        string toReplace = tokens[1];
+                        string replaceWith = tokens[2];
+
+                        if (!password.Contains(toReplace))
+                            {
+                            Console.WriteLine("Nothing to replace!");
+                        }else
+                        {
+                            password = password.Replace(toReplace, replaceWith);
+                            Console.WriteLine(password);
+                        }
+                        break;
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Your password is: {password}");
+        }
+    }
+}
---
+++ Final Exam/02.EmojiDetectorRegex/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace _02.EmojiDetectorRegex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+            // valid :::Joy::, **Banana**, ::Wink:: InValid:::Joy**, ::fox:es:, **Monk3ys**, :Snak::Es::
+            string emojiPattern = @"(?<separator>\*|:){2}(?<emoji>[A-Z][a-z]{2,})\k<separator>{2}";
+
+            string numbersPattern = @"[0-9]";
+
+            Dictionary<string, int> EmojiDictionary = new Dictionary<string, int>();
+            BigInteger coolThreshHold = 1;
+
+            MatchCollection mathesEmoji = Regex.Matches(text, emojiPattern);
+            MatchCollection mathesNumbers = Regex.Matches(text, numbersPattern);
+
+            foreach (Match match in mathesNumbers)
+            {
+                coolThreshHold *= int.Parse(match.ToString());
+
+            }
+            Console.WriteLine($"Cool threshold: { coolThreshHold}");
+            foreach (Match match in mathesEmoji)
+            {
+                string emojiName = match.ToString();
+                EmojiDictionary.Add(emojiName, 0);
+                int emojiPower = 0;
+
+                for (int i = 2; i < emojiName.Length - 2; i++)
+                {
+
+                    emojiPower += (int)emojiName[i];
+                }
+
+                EmojiDictionary[emojiName] = emojiPower;
+                //  Console.WriteLine($"{emojiName} \n {emojiPower}");
+                // Console.WriteLine(EmojiList[emojiName]);
+            }
+            Console.WriteLine($"{mathesEmoji.Count} emojis found in the text. The cool ones are:");
+            //EmojiDictionary = EmojiDictionary.OrderBy(x => x.Value).ToDictionary(x => x.Key, x => x.Value); няма нужда да са по ред
+            foreach (var emoji in EmojiDictionary)
+            {
+                if (emoji.Value >= coolThreshHold)
+                {
+                    Console.WriteLine(emoji.Key);
+                }
+
+            }
+        }
+
+    }
+}
+
