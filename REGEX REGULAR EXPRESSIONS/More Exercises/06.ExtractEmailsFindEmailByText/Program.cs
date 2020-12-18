e83ad583674a8c44b1d1facd91d6c0e0992170be
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 16:42:37 +0200

    Add files via upload

---
+++ REGEX REGULAR EXPRESSIONS/More Exercises/06.ExtractEmailsFindEmailByText/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _06.ExtractEmailsFindEmailByText
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(?<=\s)(?<user>[A-Za-z0-9]+[.-]*\w*)*@(?<hosts>[a-z]+?([.-][a-z]*)*(\.[a-z]{2,}))";
+
+            MatchCollection mathes = Regex.Matches(input, pattern);
+
+            Console.WriteLine(String.Join(Environment.NewLine, mathes));
+
+        }
+    }
+}
