3ffb05e7928cc65e36e03a4ee2db54ad88d2384a
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 16:38:31 +0200

    Add files via upload

---
+++ REGEX REGULAR EXPRESSIONS/Labs/ReplaceMath/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace ReplaceMath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //REPLACE "Nakov: 123, Branson: 456"; s// Nakov: 999, Branson: 999
+            string text = "Nakov: 123, Branson: 456";
+            string pattern = @"\d{3}";
+            string replacement = "999";
+
+            Regex regex = new Regex(pattern);
+            string result = regex.Replace(text, replacement);
+
+            Console.WriteLine(result);
+            // Nakov: 999, Branson: 999
+
+
+            //Split "1   2 3      4";  v // 1, 2, 3, 4
+            string input = "1   2 3      4";
+            string thePattern = @"\s+"; // split po edin ili pove4e whiteSpace
+
+            string[] arraySplitted = Regex.Split(input, thePattern);
+            Console.WriteLine(string.Join(", ", arraySplitted));
+            // 1, 2, 3, 4
+
+        }
+    }
+}
---
+++ REGEX REGULAR EXPRESSIONS/Labs/SoftUniLab/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace SoftUniLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string pattern = @"[A-Z][a-z]+ [A-Z][a-z]+";
+            Regex regex = new Regex(pattern);
+            // Ime Imevo  Pesho Petrov Ivan Andonov Ivanov 
+            string input = Console.ReadLine();
+
+            Console.WriteLine(regex.IsMatch(input));  // проверява дали има поне един match  на pattern-a с inputa // връща true ili false
+            Console.WriteLine(regex.Match(input).Value); // връща първия match:  Ime Imevo
+            Console.WriteLine(regex.Matches(input).Count); //Дава лист  и на листа Count - броя на намиранията
+
+            MatchCollection matches = regex.Matches(input);
+            foreach(Match match in matches)  // Изрично  във foreach  ( Match  xxxxx in xxxx)
+            {
+                Console.WriteLine(match.Value); // Ivan Ivanov
+                Console.WriteLine(match.Index);  // Index : 0   , 11 ,24
+                Console.WriteLine(match.Length);   // дължина на match
+        //   Console.WriteLine($"Ime:{match.Groups[1].Value} Familiq: {match.Groups[2].Value}");  // като масив дава първата група, може [1] [2]
+              //   Console.WriteLine(match.Groups.Count);
+            }
+        }
+    }
+}
