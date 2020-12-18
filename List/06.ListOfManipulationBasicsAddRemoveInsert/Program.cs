cd032de4eb5e5f9ae19c59e0bfbf3d06fbec2e7c
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 15:39:40 +0200

    Add files via upload

---
+++ List/06.ListOfManipulationBasicsAddRemoveInsert/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+
+namespace _06.ListOfManipulationBasicsAddRemoveInsert
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            string command = Console.ReadLine();
+
+            while(command != "end")
+            {
+                string[] operation = command.Split(" ");
+              
+                if(operation[0]== "Add")
+                {
+
+                    list.Add(int.Parse(operation[1]));
+                }
+               else if(operation[0] == "Remove")
+                {
+
+                    list.Remove((int.Parse(operation[1])));
+                }
+                else if(operation[0] == "RemoveAt")
+                {
+                    list.RemoveAt(int.Parse(operation[1]));              
+                }
+                else if(operation[0] == "Insert")
+                {
+                    list.Insert((int.Parse(operation[2])),(int.Parse(operation[1])));
+                }
+
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine(String.Join(" ",list));
+        
+        }
+    }
+}
---
+++ List/07.ListOfManipulationAdvanced/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.ListOfManipulationAdvanced
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> oldList = list;
+
+            string command = Console.ReadLine();
+
+            while (command != "end")
+            {
+                string[] operations = command.Split();
+
+                if (operations[0] == "Contains")
+                {
+                    if (list.Contains(int.Parse(operations[1])))
+                    {
+                        Console.WriteLine("Yes");
+                    }
+                    else
+                    {
+                        Console.WriteLine("No such number");
+                    }
+                }
+                else if (operations[0] == "PrintEven")
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] % 2 == 0)
+                        {
+                            Console.Write($"{list[i]} ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                else if (operations[0] == "PrintOdd")
+                {
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        if (list[i] % 2 != 0)
+                        {
+                            Console.Write($"{list[i]} ");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                else if (operations[0] == "GetSum")
+                {
+                    int sum = 0;
+                    for (int i = 0; i < list.Count; i++)
+                    {
+                        sum += i;
+                    }
+                    Console.WriteLine(sum);
+                }
+                else if (operations[0] == "Filter")
+                {
+                    string symbol = operations[1];
+                    int filter = int.Parse(operations[2]);
+                    if (symbol == "<")
+                    {
+                        Console.WriteLine(string.Join(" ", list.FindAll(x => x < filter)));
+                    }
+                    else if (symbol == ">")
+                    {
+                        Console.WriteLine(string.Join(" ", list.FindAll(x => x > filter)));
+                    }
+                    else if (symbol == ">=")
+                    {
+                        // result = numbersMore.Where(x => x >= filter).ToList();
+                        Console.WriteLine(string.Join(" ", list.FindAll(x => x >= filter)));
+                    }
+                    else if (symbol == "<=")
+                    {
+                        Console.WriteLine(string.Join(" ", list.FindAll(x => x <= filter)));
+                    }
+                }
+
+
+
+                command = Console.ReadLine();
+            }
+            if (oldList != list)
+            {
+                Console.WriteLine(String.Join(" ", list));
+            }
+        }
+    }
+}
