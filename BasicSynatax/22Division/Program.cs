0d768a8ae3d77061cd7b19726fe0acde58a36fdf
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 14:30:28 +0200

    Add files via upload

---
+++ BasicSynatax/22Division/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _22Division
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+
+            string result = string.Empty;
+
+            if (inputNumber % 10 == 0)
+            {
+                result = "The number is divisible by 10";
+            }
+            else if (inputNumber % 7 == 0)
+            {
+                result = "The number is divisible by 7";
+            }
+            else if (inputNumber % 6 == 0)
+            {
+                result = "The number is divisible by 6";
+            }
+            else if (inputNumber % 3 == 0)
+            {
+                result = "The number is divisible by 3";
+            }
+            else if (inputNumber % 2 == 0)
+            {
+                result = "The number is divisible by 2";
+            }
+            else
+            {
+                result = "Not divisible";
+
+            }
+                Console.WriteLine(result);
+        }
+    }
+}
---
+++ BasicSynatax/23Vacation/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+
+namespace _23Vacation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+            string typeOfGroup = Console.ReadLine();
+            string day = Console.ReadLine();
+
+            double totalPrice = 0;
+
+
+            if (typeOfGroup == "Students")
+            {
+                if (day == "Friday")
+                {
+
+                    totalPrice = numberOfPeople * 8.45;
+                }
+                else if (day == "Saturday")
+                {
+
+                    totalPrice = numberOfPeople * 9.80;
+                }
+                if (day == "Sunday")
+                {
+
+                    totalPrice = numberOfPeople * 10.46;
+                }
+                if (numberOfPeople >= 30)
+                {
+                    totalPrice *= 0.85;
+                }
+
+            }
+            else if (typeOfGroup == "Business")
+            {
+                if (numberOfPeople >= 100)
+                {
+                    numberOfPeople -= 10;
+                }
+
+                if (day == "Friday")
+                {
+
+                    totalPrice = numberOfPeople * 10.90;
+                }
+                else if (day == "Saturday")
+                {
+
+                    totalPrice = numberOfPeople * 15.60;
+                }
+                if (day == "Sunday")
+                {
+
+                    totalPrice = numberOfPeople * 16.0;
+                }
+
+            }
+            else if (typeOfGroup == "Regular")
+            {
+                if (day == "Friday")
+                {
+
+                    totalPrice = numberOfPeople * 15;
+                }
+                else if (day == "Saturday")
+                {
+
+                    totalPrice = numberOfPeople * 20;
+                }
+                else if (day == "Sunday")
+                {
+
+                    totalPrice = numberOfPeople * 22.5;
+                }
+                if (numberOfPeople >= 10 && numberOfPeople <= 20)
+                {
+                    totalPrice *= 0.95;
+
+
+                }
+            }
+
+                Console.WriteLine($"Total price: {totalPrice:F2}");
+
+            }
+        }
+    }
