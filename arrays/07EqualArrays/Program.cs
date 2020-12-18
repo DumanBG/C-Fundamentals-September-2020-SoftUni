5146229d8721ee2fa0cc01d0e170138295bfabeb
Teodor Avramov <64785271+DumanBG@users.noreply.github.com>
2020-12-18 15:00:14 +0200

    Add files via upload

---
+++ arrays/07EqualArrays/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Linq;
+
+namespace _07EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arrFirst = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] arrSecond = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int sum = 0;
+            bool equal = true;
+
+            for (int i = 0; i < arrFirst.Length; i++)
+            {
+                if (arrFirst[i] != arrSecond[i])
+
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    equal = false;
+                    break;
+                }
+                else
+                {
+                    sum += arrFirst[i];
+
+                }
+            }
+            if (equal)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+
+            }
+        }
+    }
+}
